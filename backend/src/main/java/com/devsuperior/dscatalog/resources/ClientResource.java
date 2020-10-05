0af968d393affa5189d71dfdc670b291b49fb4f9
Paulo Roberto <prbasa@gmail.com>
2020-10-04 21:34:53 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ClientResource.java
@@ -1,13 +1,14 @@
 package com.devsuperior.dscatalog.resources;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Client;
+import com.devsuperior.dscatalog.dto.ClientDTO;
 import com.devsuperior.dscatalog.services.ClientService;
 
 @RestController
@@ -18,8 +19,8 @@ public class ClientResource {
 	private ClientService service;
 
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/devsuperior/dscatalog/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.ClientDTO;
 import com.devsuperior.dscatalog.entities.Client;
 import com.devsuperior.dscatalog.repositories.ClientRepository;
 
@@ -15,9 +17,10 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
-	@Transactional(readOnly=true)
-	public List<Client> findAll() {
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 
 }
